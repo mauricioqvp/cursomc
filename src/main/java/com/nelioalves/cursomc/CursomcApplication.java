cc669d65bbd6793eb175fc896a68db16304ceec7
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-04-15 13:12:27 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.nelioalves.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.resources.domain.Categoria;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		// aqui o do professor foi usado:
+		// categoriaRepository.save(Arrays.asList(cat1, cat2));
+		// mas no meu dava erro. Pode ser por causa do JAVA 11 / JAVA 8
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
